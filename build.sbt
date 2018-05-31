826eb6df308f3a3447166e96dd6719bc64788f13
vivafoxdirector <vivafoxdirector@gmail.com>
2018-05-31 11:00:37 +0900

    Added future&promise package

--- build.sbt
+++ build.sbt
@@ -6,6 +6,11 @@ libraryDependencies ++= Seq(
     "org.scalikejdbc" %% "scalikejdbc"       % "3.2.2",
     "org.scalikejdbc" %% "scalikejdbc-config" % "3.0.0",
     "com.h2database"  %  "h2"                % "1.4.197",
-    "ch.qos.logback"  %  "logback-classic"   % "1.2.3"
+    "ch.qos.logback"  %  "logback-classic"   % "1.2.3",
+
+	// Akka
+	"com.typesafe.akka" %% "akka-actor" % "2.4.2"
+
+
 )
     
---
+++ src/main/scala/io/yian/futurenpromise/FutureSample.scala
@@ -0,0 +1,34 @@
+import scala.concurrent.Future
+import scala.concurrent.ExecutionContext.Implicits.global
+
+// https://dwango.github.io/scala_text/future-and-promise.html
+
+object FutureSample extends App {
+  val s = "Hello"
+  val f:Future[String] = Future {
+    Thread.sleep(1000)
+    s + "future!"
+  }
+
+  f.foreach {
+    case s: String => println(s)
+  }
+
+  println(f.isCompleted)  // false
+  Thread.sleep(5000)      // hello future!
+  println(f.isCompleted)  // true
+
+  val f2: Future[String] = Future {
+    Thread.sleep(1000)
+    throw new RuntimeException("일부러 그럼")
+  }
+
+  f2.failed.foreach {
+    case e: Throwable => 
+      println(e.getMessage)
+  }
+
+  println(f2.isCompleted) // false
+  Thread.sleep(5000)      // 일부러 그럼
+  println(f2.isCompleted) // true
+}
