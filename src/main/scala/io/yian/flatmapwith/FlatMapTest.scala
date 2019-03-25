547bdb09325c64af885c74903d6e98e670adb175
yongsu.jeon <yongsu.jeon@dabomsoft.com>
2019-03-25 21:57:50 +0900

    Add some exam

--- src/main/scala/io/yian/flatmapwith/FlatMapTest.scala
+++ src/main/scala/io/yian/flatmapwith/FlatMapTest.scala
@@ -1,8 +1,9 @@
 package io.yian.flatmapwith
 
 // ref: https://qiita.com/mtoyoshi/items/c95cc88de2910945c39d
-// ref: http://ym.hatenadiary.jp/entry/2016/03/30/092337
 // ref: https://dev.classmethod.jp/server-side/scala-learn-flatmap/
+// ref: http://ym.hatenadiary.jp/entry/2016/03/30/092337
+// ref: http://tolarian-academy.net/option%E5%9E%8B%E3%81%AEmap%E3%81%A8flatmap%E3%81%8C%E3%82%88%E3%81%86%E3%82%84%E3%81%8F%E3%82%8F%E3%81%8B%E3%81%A3%E3%81%9F%EF%BC%88scala%E3%81%AE%E8%A9%B1%EF%BC%89/
 // ref: https://alvinalexander.com/scala/collection-scala-flatmap-examples-map-flatten
 // ref: http://www.brunton-spall.co.uk/post/2011/12/02/map-map-and-flatmap-in-scala/
 object FlatMapTest {
--- src/main/scala/io/yian/monads/Monads.scala
+++ src/main/scala/io/yian/monads/Monads.scala
@@ -2,6 +2,7 @@ package io.yian.monads
 
 // ref: https://gist.github.com/nenono/b53dd7ce41629071a2fa
 // ref: http://eed3si9n.com/learning-scalaz/ja/Monad.html
+// ref: http://d.hatena.ne.jp/papamitra/20101002/scala_monad
 package object Monads {
 
 
